de00b3afb71b33eddb74c45964f3605a2fae9b27
probonopd <probonopd@users.noreply.github.com>
2016-10-22 22:28:09 +0200

    Fix missing "'"

--- shared/shared.cpp
+++ shared/shared.cpp
@@ -1148,7 +1148,7 @@ bool checkAppImagePrerequisites(const QString &appDirPath)
 
 int createAppImage(const QString &appDirPath)
 {
-    QString appImageCommand = "appimagetool '" + appDirPath; // +"' '" + appImagePath + "'";
+    QString appImageCommand = "appimagetool '" + appDirPath + "'"; // +"' '" + appImagePath + "'";
     int ret = system(appImageCommand.toUtf8().constData());
     return WEXITSTATUS(ret);
 }
