d2193970286fa047db3f29f23574decb6142c963
probonopd <probonopd@users.noreply.github.com>
2018-01-07 16:27:35 +0000

    Download Qt from qt.io

--- tests/tests-environment.sh
+++ tests/tests-environment.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-sudo add-apt-repository --yes ppa:beineri/opt-qt593-trusty
-sudo apt-get update -qq
-
 wget https://ftp.fau.de/debian/pool/main/p/patchelf/patchelf_0.8-2_amd64.deb
 echo "5d506507df7c02766ae6c3ca0d15b4234f4cb79a80799190ded9d3ca0ac28c0c  patchelf_0.8-2_amd64.deb" | sha256sum -c
 sudo dpkg -i patchelf_0.8-2_amd64.deb
@@ -16,4 +13,9 @@ chmod +x appimagetool*AppImage
 sudo cp squashfs-root/usr/bin/* /usr/local/bin
 cd -
 
-sudo apt-get -y install qt59base qt59declarative qt59webengine binutils xpra zsync desktop-file-utils
+sudo apt-get -y install binutils xpra zsync desktop-file-utils
+
+# Download Qt from qt.io
+. qtdownload
+QTSUBMODULE=qtdeclarative ; . qtdownload
+QTCOMPONENT=qt.qt5.5100.qtwebengine.gcc_64 ;  QTSUBCOMPONENT=qtwebengine ; . qtdownload
