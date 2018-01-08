396eb754e37364faa7beb5c5f06dbc83230340b7
probonopd <probonopd@users.noreply.github.com>
2018-01-08 03:16:29 +0000

    Update travis.sh

--- tests/travis.sh
+++ tests/travis.sh
@@ -24,9 +24,10 @@ wget -c "http://download.qt.io/official_releases/online_installers/qt-unified-li
 chmod a+x ./qt-unified-linux-x64-online.run
 export QT_QPA_PLATFORM=minimal
 ./qt-unified-linux-x64-online.run --script qt-installer-noninteractive.qs --no-force-installations --verbose
-export CMAKE_PREFIX_PATH=~/Qt/*/gcc_64/lib/cmake
-export LD_LIBRARY_PATH=~/Qt/*/gcc_64/lib
-export PATH=~/Qt/*/gcc_64/bin/:${PATH}
+export CMAKE_PREFIX_PATH=$(readlink -f ~/Qt/*/gcc_64/lib/cmake)
+export LD_LIBRARY_PATH=$(readlink -f~/Qt/*/gcc_64/lib)
+export PATH=$(readlink -f~/Qt/*/gcc_64/bin/):${PATH}
+qmake -v
 
 qmake CONFIG+=release CONFIG+=force_debug_info linuxdeployqt.pro
 make -j
