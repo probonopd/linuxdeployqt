bf2a67ead08d194c70393d3f84622ae5735684ee
TheAssassin <theassassin@assassinate-you.net>
2018-01-13 21:38:41 +0100

    Try to fix build issues (#214)

--- tests/tests-environment.sh
+++ tests/tests-environment.sh
@@ -15,6 +15,7 @@ chmod +x appimagetool*AppImage
 ./appimagetool*AppImage --appimage-extract
 sudo cp squashfs-root/usr/bin/* /usr/local/bin/
 sudo cp -r squashfs-root/usr/lib/appimagekit /usr/local/lib/
+sudo chmod +rx /usr/local/lib/appimagekit
 cd -
 
-sudo apt-get -y install qt59base qt59declarative qt59webengine binutils xpra zsync desktop-file-utils
+sudo apt-get -y install qt59base qt59declarative qt59webengine binutils xpra zsync desktop-file-utils gcc g++ make libgl1-mesa-dev fuse psmisc qt59translations
