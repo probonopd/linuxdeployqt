7b7f603278d1f64b18457e7537a83fc3c38f8ebd
Patrick José Pereira <patrickelectric@gmail.com>
2018-04-09 20:49:58 -0300

    excludelist: Return empty string if something goes wrong
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tools/excludelist.sh
+++ tools/excludelist.sh
@@ -2,6 +2,12 @@
 
 # Download excludelist
 blacklisted=($(wget --quiet https://raw.githubusercontent.com/probonopd/AppImages/master/excludelist -O - | sort | uniq | grep -v "^#.*" | grep "[^-\s]"))
+if [ -z $blacklisted ]; then
+    # Return nothing if no output from command
+    echo ""
+    exit
+fi
+
 # Create array
 for item in ${blacklisted[@]:0:${#blacklisted[@]}-1}; do
   echo -ne '\\"'$item'\\" << '
