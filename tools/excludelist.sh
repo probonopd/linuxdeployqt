ba3495b85585eac6ecb79c3334ab894c9630f078
TheAssassin <theassassin@assassinate-you.net>
2018-04-12 02:54:08 +0200

    Fix exit code ot excludelist.sh

--- tools/excludelist.sh
+++ tools/excludelist.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
+set -e
+
 # Download excludelist
 blacklisted=($(wget --quiet https://raw.githubusercontent.com/probonopd/AppImages/master/excludelist -O - | sort | uniq | grep -v "^#.*" | grep "[^-\s]"))
-if [ -z $blacklisted ]; then
-    # Return nothing if no output from command
-    echo ""
-    exit
-fi
 
 # Create array
 for item in ${blacklisted[@]:0:${#blacklisted[@]}-1}; do
