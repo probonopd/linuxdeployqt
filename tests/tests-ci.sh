8e31fd6c01343ae36a08b8ea859f204ed44e9440
probonopd <probonopd@users.noreply.github.com>
2019-07-28 18:40:54 +0000

    Update tests-ci.sh

--- tests/tests-ci.sh
+++ tests/tests-ci.sh
@@ -13,7 +13,7 @@ sudo apt-get update
 sudo apt-get -y install --no-install-recommends pvs-studio
 pvs-studio-analyzer credentials probono@puredarwin.org $PVS_KEY -o ./licence.lic
 pvs-studio-analyzer trace -- make -j$(nproc)
-pvs-studio-analyzer analyze -e /opt -e /usr -o pvs-studio.log -e ./src -j 4 -l ./licence.lic
+pvs-studio-analyzer analyze -e /opt -e /usr -o pvs-studio.log -j $(nproc) -l ./licence.lic
 plog-converter -a GA:1,2 -t tasklist -o pvs-studio-report.txt pvs-studio.log
 rm ./licence.lic
 
