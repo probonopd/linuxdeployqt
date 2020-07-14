3108349a110dcdc59b2fe5e7a193e87f93803e09
Nils Schimmelmann <nschimme@users.noreply.github.com>
2020-07-14 11:09:57 -0700

    Update env for arm64

--- tests/tests-environment.sh
+++ tests/tests-environment.sh
@@ -2,24 +2,34 @@
 
 set -e
 
-sudo add-apt-repository --yes ppa:beineri/opt-qt593-trusty
+export ARCH=`arch`
+if [ "$ARCH" == "amd64" ]; then
+  export PPA=ppa:beineri/opt-qt593-trusty
+fi
+if [ "$ARCH" == "arm64" ]; then
+  export PPA=ppa:adix-me/opt-qt594-xenial-arm64
+fi
+
+sudo add-apt-repository --yes $PPA
 sudo apt-get update -qq
 
-wget https://ftp.fau.de/debian/pool/main/p/patchelf/patchelf_0.8-2_amd64.deb
-echo "5d506507df7c02766ae6c3ca0d15b4234f4cb79a80799190ded9d3ca0ac28c0c  patchelf_0.8-2_amd64.deb" | sha256sum -c
-sudo dpkg -i patchelf_0.8-2_amd64.deb
-# We want a newer patchelf since the one above is missing e.g., '--add-needed' which our users might want to use
-# However, the newer patchelf versions are broken and cripple, e.g., libQt5Core; see https://github.com/NixOS/patchelf/issues/124
-# git clone -o e1e39f3 https://github.com/NixOS/patchelf
-# cd patchelf
-# bash ./bootstrap.sh
-# ./configure --prefix=/usr
-# make -j$(nproc)
-# sudo make install
+if [ "$ARCH" == "amd64" ]; then
+  wget https://ftp.fau.de/debian/pool/main/p/patchelf/patchelf_0.8-2_$ARCH.deb
+  echo "5d506507df7c02766ae6c3ca0d15b4234f4cb79a80799190ded9d3ca0ac28c0c  patchelf_0.8-2_$ARCH.deb" | sha256sum -c
+ sudo dpkg -i patchelf_0.8-2_$ARCH.deb
+  # We want a newer patchelf since the one above is missing e.g., '--add-needed' which our users might want to use
+  # However, the newer patchelf versions are broken and cripple, e.g., libQt5Core; see https://github.com/NixOS/patchelf/issues/124
+  # git clone -o e1e39f3 https://github.com/NixOS/patchelf
+  # cd patchelf
+  # bash ./bootstrap.sh
+  # ./configure --prefix=/usr
+  # make -j$(nproc)
+  # sudo make install
+fi
 
 cd /tmp/
 # wget -c https://artifacts.assassinate-you.net/artifactory/AppImageKit/travis-2052/appimagetool-x86_64.AppImage # branch last-good, https://travis-ci.org/AppImage/AppImageKit/jobs/507462541
-wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-$ARCH.AppImage"
 chmod +x appimagetool*AppImage
 ./appimagetool*AppImage --appimage-extract
 sudo cp squashfs-root/usr/bin/* /usr/local/bin/
