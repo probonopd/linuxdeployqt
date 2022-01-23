adf1eb084055e68bd7f62a47273fed5291eda9f2
Adrien RICCIARDI <RICCIARDI-Adrien@users.noreply.github.com>
2022-01-23 11:24:23 +0100

    Added GitHub Action CI. (#518)
    * Added GitHub Action CI.
    
    Signed-off-by: RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
    
    * Removed Travis CI support.
    
    Signed-off-by: RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>

--- tests/tests-ci.sh
+++ tests/tests-ci.sh
@@ -4,18 +4,18 @@ set -x
 
 source /opt/qt*/bin/qt*-env.sh
 /opt/qt*/bin/qmake CONFIG+=release CONFIG+=force_debug_info linuxdeployqt.pro
-# make -j$(nproc) # Not doing here but below with "pvs-tool trace"
+make -j$(nproc)
 
-# Test
-wget -q -O - http://files.viva64.com/etc/pubkey.txt | sudo apt-key add -
-sudo wget -O /etc/apt/sources.list.d/viva64.list http://files.viva64.com/etc/viva64.list
-sudo apt-get update
-sudo apt-get -y install --no-install-recommends pvs-studio
-pvs-studio-analyzer credentials probono@puredarwin.org $PVS_KEY -o ./licence.lic
-pvs-studio-analyzer trace -- make -j$(nproc)
-pvs-studio-analyzer analyze -e /opt -e /usr -o pvs-studio.log -j $(nproc) -l ./licence.lic
-plog-converter -a GA:1,2 -t tasklist -o pvs-studio-report.txt pvs-studio.log
-rm ./licence.lic
+# Test (it is commented out because current PVS Studio license is expired, it might be re-enabled later if a new license is granted)
+#wget -q -O - http://files.viva64.com/etc/pubkey.txt | sudo apt-key add -
+#sudo wget -O /etc/apt/sources.list.d/viva64.list http://files.viva64.com/etc/viva64.list
+#sudo apt-get update
+#sudo apt-get -y install --no-install-recommends pvs-studio
+#pvs-studio-analyzer credentials probono@puredarwin.org $PVS_KEY -o ./licence.lic
+#pvs-studio-analyzer trace -- make -j$(nproc)
+#pvs-studio-analyzer analyze -e /opt -e /usr -o pvs-studio.log -j $(nproc) -l ./licence.lic
+#plog-converter -a GA:1,2 -t tasklist -o pvs-studio-report.txt pvs-studio.log
+#rm ./licence.lic
 
 # exit on failure
 set -e
@@ -26,8 +26,7 @@ cp ./bin/linuxdeployqt linuxdeployqt.AppDir/usr/bin/
 cp -r /usr/local/lib/appimagekit linuxdeployqt.AppDir/usr/lib/
 chmod +x linuxdeployqt.AppDir/AppRun
 find linuxdeployqt.AppDir/
-export VERSION=continuous
-if [ ! -z $TRAVIS_TAG ] ; then export VERSION=$TRAVIS_TAG ; fi
+if [ -z "$VERSION" ] ; then export VERSION=continuous ; fi
 ./bin/linuxdeployqt linuxdeployqt.AppDir/linuxdeployqt.desktop -verbose=3 -appimage \
     -executable=linuxdeployqt.AppDir/usr/bin/desktop-file-validate
 ls -lh
@@ -43,7 +42,7 @@ do
 done
 
 # enable core dumps
-echo "/tmp/coredump" | sudo tee /proc/sys/kernel/core_pattern
+#echo "/tmp/coredump" | sudo tee /proc/sys/kernel/core_pattern
 
 ulimit -c unlimited
 ulimit -a -S
@@ -52,8 +51,8 @@ ulimit -a -H
 # error handling performed separately
 set +e
 
-# print version number
-./linuxdeployqt-*-x86_64.AppImage --version
+# print version number (need to extract the AppImage because we are running in a container, see https://github.com/AppImage/AppImageKit/wiki/FUSE#docker)
+./linuxdeployqt-*-x86_64.AppImage --appimage-extract-and-run --version
 
 # TODO: reactivate tests
 #bash -e tests/tests.sh
